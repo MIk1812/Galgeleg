b5bbf7ceb36b31774c93ab6a133cf92245606633
Mik1812 <s183913@student.dtu.dk>
2020-10-25 19:48:09 +0100

    Initial commit

---
+++ app/src/main/java/com/example/galgeleg/ActStart.java
@@ -0,0 +1,14 @@
+package com.example.galgeleg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActStart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_start);
+    }
+}
